cbb1d7c9e96f4fdef160d1130c08721d87a3fd5a
SNEHA <reachsneha02@gmail.com>
2025-04-28 00:27:43 -0400

    Update assignment2.sql

--- 02_activities/assignments/assignment2.sql
+++ 02_activities/assignments/assignment2.sql
@@ -99,7 +99,7 @@ HINT: There are a possibly a few ways to do this query, but if you're struggling
 "best day" and "worst day"; 
 3) Query the second temp table twice, once for the best day, once for the worst day, 
 with a UNION binding them. */
-DROP TABLE IF EXISTS sales_per_day;
+
 CREATE TEMP TABLE sales_per_day AS 
 SELECT 
 market_date, 
